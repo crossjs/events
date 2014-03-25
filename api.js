1c7b5a0932bd4e279a4133d2106e1969f6c101cb
crossjs <one@ffully.com>
2014-03-25 11:29:15 +0800

    update api doc

---
+++ api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Events"
+    ],
+    "modules": [
+        "Events"
+    ],
+    "allModules": [
+        {
+            "displayName": "Events",
+            "name": "Events",
+            "description": "事件"
+        }
+    ]
+} };
+});
